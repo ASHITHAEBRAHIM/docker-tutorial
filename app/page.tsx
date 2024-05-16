b4bc709242e9d9f5f4fe37d0f33d0c32dd682484
Ashitha Ebrahim <achuashitha5@gmail.com>
2024-05-16 14:16:07 +0530

    push changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-5xl text-blue-600 font-bold ">Happy Birthday Docker!</h1>
+        <h1 className="text-5xl text-blue-600 font-bold ">Hello Docker!</h1>
         <Image src='/docker.png' height={250} width={250} alt='docker'/>
         <p className="text-black">
         Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.The service has both free and premium tiers. The software that hosts the containers is called Docker Engine.[6] It was first released in 2013 and is developed by Docker,
